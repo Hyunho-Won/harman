6bbcfabd95b6f78fa720bee0b57d137c683ac3f4
Hyunho Won <122242141+Hyunho-Won@users.noreply.github.com>
2023-02-21 17:03:42 +0900

    Add files via upload

---
+++ code/23.02.21/driver/Led/Led.c
@@ -0,0 +1,27 @@
+﻿/*
+ * Led.c
+ *
+ * Created: 2023-02-21 오전 11:52:26
+ *  Author: rhoblack
+ */ 
+#include "Led.h"
+
+void Led_initPort(volatile uint8_t *DDR)
+{
+	Gpio_initPort(DDR, OUTPUT);
+}
+
+void Led_writePort(volatile uint8_t *PORT, uint8_t data)
+{
+	Gpio_writePort(PORT, data);
+}
+
+void Led_allOn(volatile uint8_t *PORT)
+{
+	Gpio_writePort(PORT, 0xff);
+}
+
+void Led_allOff(volatile uint8_t *PORT)
+{
+	Gpio_writePort(PORT, 0x00);
+}
---
+++ code/23.02.21/driver/Led/Led.h
@@ -0,0 +1,21 @@
+﻿/*
+ * Led.h
+ *
+ * Created: 2023-02-21 오전 11:52:36
+ *  Author: rhoblack
+ */ 
+
+
+#ifndef LED_H_
+#define LED_H_
+
+#include <avr/io.h>
+#include "../../periph/GPIO/Gpio.h"
+
+void Led_initPort(volatile uint8_t *DDR);
+void Led_writePort(volatile uint8_t *PORT, uint8_t data);
+void Led_allOn(volatile uint8_t *PORT);
+void Led_allOff(volatile uint8_t *PORT);
+
+
+#endif /* LED_H_ */
