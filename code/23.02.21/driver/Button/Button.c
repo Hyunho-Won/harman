b8e636610c40de708ef5faaaa2a5762665d51fc7
Hyunho Won <122242141+Hyunho-Won@users.noreply.github.com>
2023-02-21 17:02:51 +0900

    Add files via upload

---
+++ code/23.02.21/driver/Button/Button.c
@@ -0,0 +1,32 @@
+﻿/*
+ * Button.c
+ *
+ * Created: 2023-02-21 오전 11:12:30
+ *  Author: rhoblack
+ */ 
+#include "Button.h"
+
+void Button_init(button_t *btn, volatile uint8_t *DDR, volatile uint8_t *PIN, uint8_t pinNum)
+{
+	btn->DDR = DDR;
+	btn->PIN = PIN;
+	btn->pinNum = pinNum;
+	btn->prevState = RELEASED;
+	Gpio_initPin(btn->DDR, btn->pinNum, INPUT);
+}
+
+uint8_t Button_getState(button_t *btn)
+{
+	uint8_t curState = Gpio_readPin(btn->PIN, btn->pinNum);
+	if ( (curState == PUSHED) && (btn->prevState == RELEASED) ) {
+		_delay_ms(10); // debounce code
+		btn->prevState = PUSHED;
+		return ACT_PUSHED;
+	}
+	else if ( (curState != PUSHED) && (btn->prevState == PUSHED) ){
+		_delay_ms(10); // debounce code
+		btn->prevState = RELEASED;
+		return ACT_RELEASED;
+	}
+	return ACT_NONE;
+}
---
+++ code/23.02.21/driver/Button/Button.h
@@ -0,0 +1,33 @@
+﻿/*
+ * Button.h
+ *
+ * Created: 2023-02-21 오전 11:12:42
+ *  Author: rhoblack
+ */ 
+
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#define F_CPU	16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../../periph/GPIO/Gpio.h"
+
+enum {PUSHED, RELEASED};
+enum {ACT_NONE, ACT_PUSHED, ACT_RELEASED};
+
+typedef struct _button {
+	volatile uint8_t *DDR;
+	volatile uint8_t *PIN;
+	uint8_t pinNum;
+	uint8_t prevState;
+}button_t;
+
+void Button_init(button_t *btn, volatile uint8_t *DDR, volatile uint8_t *PIN, uint8_t pinNum);
+uint8_t Button_getState(button_t *btn);
+
+
+
+
+#endif /* BUTTON_H_ */
